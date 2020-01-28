02022a945cd344c754397b6a110082358ed29a79
Frank Wei <yw1685@nyu.edu>
2020-01-28 16:30:11 -0500

    Updates

---
+++ precache-manifest.eb81dc5ff0d2814cfe0c1f09b27f6e4e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c21b63f0306084b5c746a5530fb15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c43df89c119701f747d",
+    "url": "/static/css/main.1c800ce8.chunk.css"
+  },
+  {
+    "revision": "2cf233259edd736c8c82",
+    "url": "/static/js/2.204cb435.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.204cb435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c43df89c119701f747d",
+    "url": "/static/js/main.cca131e9.chunk.js"
+  },
+  {
+    "revision": "1f149feb0ed8e0770972",
+    "url": "/static/js/runtime-main.e32a4d24.js"
+  },
+  {
+    "revision": "e392ea64532f13f8f55636ffbeaaa539",
+    "url": "/static/media/article.e392ea64.svg"
+  },
+  {
+    "revision": "71118df54d30cf025784c6db5340b8c4",
+    "url": "/static/media/flickr.71118df5.svg"
+  },
+  {
+    "revision": "345b0e3a9e8898ddf9f8d50c9df667f4",
+    "url": "/static/media/github.345b0e3a.svg"
+  },
+  {
+    "revision": "eae853e29f4800d93e23e5f8244b1667",
+    "url": "/static/media/liftspotter_account.eae853e2.png"
+  },
+  {
+    "revision": "dfb7cc7724e412b0571c50bb751f06fb",
+    "url": "/static/media/liftspotter_addroutine.dfb7cc77.png"
+  },
+  {
+    "revision": "148f9be0e14161b5445cf72899a64ed9",
+    "url": "/static/media/liftspotter_addworkout.148f9be0.png"
+  },
+  {
+    "revision": "f6876a53efd5c3b01b27071ee73162a8",
+    "url": "/static/media/liftspotter_community.f6876a53.png"
+  },
+  {
+    "revision": "a0d78895a248ccd76b396f8f24291c7b",
+    "url": "/static/media/liftspotter_login.a0d78895.png"
+  },
+  {
+    "revision": "57126326cc0351c7945fd7edc8bfaf6e",
+    "url": "/static/media/linkedin.57126326.svg"
+  },
+  {
+    "revision": "aad3c0c02c04f5f9b102e2f5a578f1cf",
+    "url": "/static/media/littleeinstein_cardsorting.aad3c0c0.png"
+  },
+  {
+    "revision": "80cba3e72be0beac3503124bc2da46eb",
+    "url": "/static/media/littleeinstein_interactive.80cba3e7.png"
+  },
+  {
+    "revision": "cc4d7e6eada148fe14b54a31d1598efe",
+    "url": "/static/media/littleeinstein_main.cc4d7e6e.png"
+  },
+  {
+    "revision": "86b0529d40a0c974edb58d20c96c189e",
+    "url": "/static/media/littleeinstein_mindmap.86b0529d.png"
+  },
+  {
+    "revision": "4329e528e0b3a1f4c54a6845a1600b93",
+    "url": "/static/media/littleeinstein_paperproto.4329e528.png"
+  },
+  {
+    "revision": "f5e3c2aa2a501288429e83e558ce4235",
+    "url": "/static/media/littleeinstein_userpersona.f5e3c2aa.png"
+  },
+  {
+    "revision": "586c73c5ad22d4810feab768a8d92d4a",
+    "url": "/static/media/oneplaylist_get.586c73c5.png"
+  },
+  {
+    "revision": "14094c184c84408da5e416acd3f65921",
+    "url": "/static/media/oneplaylist_mapping.14094c18.png"
+  },
+  {
+    "revision": "906ebbdecfd0ee58c958ad49f59b8f7b",
+    "url": "/static/media/portrait.906ebbde.jpg"
+  },
+  {
+    "revision": "be2f00c8e3ad85f49abf76d966c8d04d",
+    "url": "/static/media/qrfound_login.be2f00c8.png"
+  },
+  {
+    "revision": "5b9554b6040404e63ad44d937b527e4e",
+    "url": "/static/media/qrfound_main.5b9554b6.png"
+  },
+  {
+    "revision": "ff06add07d124a98b10f135cf9a82165",
+    "url": "/static/media/qrfound_sharding.ff06add0.png"
+  },
+  {
+    "revision": "4848c270d308c0ddb01dec432d1de88c",
+    "url": "/static/media/qrfound_update.4848c270.png"
+  },
+  {
+    "revision": "db2b49958b1e3704415e56245f90c936",
+    "url": "/static/media/scroll_up_icon.db2b4995.svg"
+  },
+  {
+    "revision": "7ca5a9b65d5fe38dcefb470b61322818",
+    "url": "/static/media/soundbyte_cover.7ca5a9b6.jpg"
+  },
+  {
+    "revision": "31f6c8af8a1f368c4a28455c7d4e5321",
+    "url": "/static/media/soundbyte_hearingbrain.31f6c8af.jpg"
+  },
+  {
+    "revision": "c4c4d00c8f7a70f53b0d04308c67995d",
+    "url": "/static/media/soundbyte_interview.c4c4d00c.jpg"
+  },
+  {
+    "revision": "287284fc54dec0c283dc682429bed44b",
+    "url": "/static/media/soundbyte_prototype.287284fc.jpg"
+  },
+  {
+    "revision": "6d763c4a7265e443bcbc4931c9a1809a",
+    "url": "/static/media/wireframe.6d763c4a.svg"
+  }
+]);
